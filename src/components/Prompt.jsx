68867e702b6f5190faa84bbf1df1078e09e34a0f
Asai Toshiya <to.asai.60@gmail.com>
2022-11-01 12:11:32 +0900

    未使用の関数を削除する

--- src/components/Prompt.jsx
+++ src/components/Prompt.jsx
@@ -3,56 +3,6 @@ import { createSignal } from "solid-js";
 function Prompt(props) {
   const [keyword, setKeyword] = createSignal("");
 
-  const addKeyword = (keyword, divId, textareaId) => {
-    const div = document.getElementById(divId);
-    const button = createButton(keyword.trim(), divId, textareaId);
-    div.appendChild(button);
-    div.appendChild(document.createTextNode("\n"));
-  };
-
-  const addKeywords = (keywords, divId, textareaId) => {
-    keywords.forEach((keyword) => addKeyword(keyword, divId, textareaId));
-  };
-
-  const createButton = (keyword, divId, textareaId) =>
-    Object.assign(document.createElement("button"), {
-      className: "keyword",
-      innerText: keyword,
-      onclick: ({ target }) => {
-        handleRemove(target, divId, textareaId);
-      },
-    });
-
-  const createInput = (id) =>
-    Object.assign(document.createElement("input"), {
-      id,
-      type: "text",
-    });
-
-  const createTextarea = (id) =>
-    Object.assign(document.createElement("textarea"), {
-      id,
-      readonly: true,
-    });
-
-  const replaceNode = (newNode, oldNode) => {
-    oldNode.parentNode.replaceChild(newNode, oldNode);
-  };
-
-  const toKeywordArray = (divId) => {
-    const div = document.getElementById(divId);
-    return [...div.children].map((child) => child.innerText);
-  };
-
-  const update = (divId, textareaId) => {
-    const newTextarea = createTextarea(textareaId);
-    newTextarea.appendChild(
-      document.createTextNode(toKeywordArray(divId).join(", "))
-    );
-    const textarea = document.getElementById(textareaId);
-    replaceNode(newTextarea, textarea);
-  };
-
   const handleAdd = () => {
     props.onAdd?.(keyword());
     setKeyword("");
